e6c142f08a9341b9a6874dba3bba70f836bdf6c7
mechnotech <eashumilov@fintechiq.ru>
2025-01-30 16:18:52 +0300

    fix message model

--- src/models/messages_models.py
+++ src/models/messages_models.py
@@ -1,17 +1,30 @@
+import json
 from datetime import datetime
 from typing import Optional
 
 from pydantic import BaseModel, field_validator, Field
 
 
+
 class SMSMessage(BaseModel):
     sender: str
     content: str
     received_date: datetime = Field(alias='date')
-    partial: Optional[dict]
+    partial: Optional[dict | str]
 
     @field_validator('partial', mode='before')
     def validate_partial(cls, v):
         if not isinstance(v, dict):
-            return None
-        return v
+            return
+        return json.dumps(v, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    package = {
+        'sender': 'me',
+        'content': 'hello!',
+        'date': datetime.now(),
+        'partial': {'key': 'value'}
+    }
+    sms = SMSMessage(**package)
+    print(sms)
